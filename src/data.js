dbd3414674108a3945527817b329038cc6ec75c7
vikas-reddy-pagilla <pagilla.vikas.reddy@gmail.com>
2022-01-14 00:14:54 +0530

    intial commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: "Stopla App",
+    subtitle: "MERN Stack",
+    description:
+      "A web application.",
+    image: "./project-1.gif",
+    link: "https://stopla.in/",
+  }
+];
